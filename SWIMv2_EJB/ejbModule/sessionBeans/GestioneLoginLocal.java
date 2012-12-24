2f18ee905df53ce23096fbaca80c8ecedce65709
Stefano Cappa <stefano.cappa@mail.polimi.it>
2012-12-24 22:17:58 +0000

    test su session login fatto e funzionante. Prima di fare ogni cosa bisogna avere i 3 progetti giusti e deployare dall'EAR (non più dal EJB) Vedere in test/logintest. Ovviamente perchè funzioni il primo asserttrue mettere nel db l'utente e la sua password hashata che viene stampata al lancio del test. Cioè: oiQurVXJTD3rfPI0C/751bysoi3+ZuZGdF7kNxxjP8g=

--- SWIMv2_EJB/ejbModule/sessionBeans/GestioneLoginLocal.java
+++ SWIMv2_EJB/ejbModule/sessionBeans/GestioneLoginLocal.java
@@ -2,7 +2,49 @@ package sessionBeans;
 
 import javax.ejb.Local;
 
+import entityBeans.Amministratore;
+import entityBeans.Utente;
+import exceptions.HashingException;
+
 @Local
 public interface GestioneLoginLocal {
+
+	/**
+	 * Metodo che controlla se il login e' stato effettuato da parte di un amministratore
+	 * 
+	 * @param email
+	 *            e' l'email inserita durante il login
+	 * @param passwordInserita
+	 *            e' la password inserita durante il login
+	 * @return true se un amministratore ha effettuato il login, false altrimenti
+	 */
+	public boolean eseguiLoginAmministratore(String email,
+			String passwordInserita) throws HashingException;
+
+	/**
+	 * Metodo che controlla se il login e' stato effettuato da parte di un utente
+	 * 
+	 * @param email
+	 *            e' l'email inserita durante il login
+	 * @param passwordInserita
+	 *            e' la password inserita durante il login
+	 * @return true se un utente ha effettuato il login, false altrimenti
+	 */
+	public boolean esegueLoginUtente(String email, String passwordInserita)
+			throws HashingException;
+
+	/**
+	 * Metodo per l'estrazione dell'amministratore dal database data la sua email
+	 * @param email e' l'email dell'amministratore
+	 * @return <b>l'amministratore</b> corrispondente all'email, se esiste, <b>null</b> altrimenti
+	 */
+	public Amministratore getAmministratoreByEmail(String email);
+
+	/**
+	 * Metodo per l'estrazione dell'utente dal database data la sua email
+	 * @param email e' l'email dell'amministratore
+	 * @return <b>l'amministratore</b> corrispondente all'email, se esiste, <b>null</b> altrimenti
+	 */
+	public Utente getUtenteByEmail(String email);
 
 }
---
+++ SWIMv2_EJB/ejbModule/utililies/sessionRemote/GestioneLoginRemote.java
@@ -0,0 +1,10 @@
+package utililies.sessionRemote;
+
+import javax.ejb.Remote;
+
+import sessionBeans.GestioneLoginLocal;
+
+@Remote
+public interface GestioneLoginRemote extends GestioneLoginLocal {
+
+}
