254f2b74e28dba623d9d09b74540700c96564c55
daven8989@gmail.com <daven8989@gmail.com@5549e029-e0ad-b569-f86b-1b750b3ebaed>
2012-12-27 16:23:29 +0000

    prima versione della GestioneAmicizie, con il metodo del suggerimento compreso.

--- trunk/SWIMv2_EJB/ejbModule/sessionBeans/interfaces/GestioneAmicizieInterface.java
+++ trunk/SWIMv2_EJB/ejbModule/sessionBeans/interfaces/GestioneAmicizieInterface.java
@@ -1,5 +1,35 @@
 package sessionBeans.interfaces;
 
+import java.util.List;
+
+import entityBeans.Amicizia;
+import entityBeans.Collaborazione;
+import entityBeans.Utente;
+
 public interface GestioneAmicizieInterface {
+
+	public abstract Collaborazione getCollaborazione(Long id);
+
+	public abstract Amicizia richiediAmicizia(String emailUtente1,
+			String emailUtente2, boolean diretta);
+
+	public abstract void accettaAmicizia(String emailUtente1,
+			String emailUtente2);
+
+	public abstract void rifiutaAmicizia(String emailUtente1,
+			String emailUtente2);
+
+	@SuppressWarnings("unchecked")
+	public abstract List<Utente> getAmici(String emailUtente);
+
+	public abstract List<Utente> getSuggerimenti(String emailUtenteAppenaAmico,
+			String emailUtente);
+
+	/**
+	 * Metodo per l'estrazione dell'utente dal database data la sua email
+	 * @param email = l'email dell'amministratore
+	 * @return <b>utente</b> corrispondente all'email, se esiste, <b>null</b> altrimenti
+	 */
+	public abstract Utente getUtenteByEmail(String email);
 
 }
