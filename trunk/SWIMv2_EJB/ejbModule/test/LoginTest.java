69b7561b0c1e787645d938da61c04f1b50a09299
stefano.cappa.ks89@gmail.com <stefano.cappa.ks89@gmail.com@5549e029-e0ad-b569-f86b-1b750b3ebaed>
2012-12-27 21:27:14 +0000

    alcune modifiche

--- trunk/SWIMv2_EJB/ejbModule/test/LoginTest.java
+++ trunk/SWIMv2_EJB/ejbModule/test/LoginTest.java
@@ -8,20 +8,24 @@ import javax.rmi.PortableRemoteObject;
 
 import junit.framework.Assert;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import utililies.PasswordHasher;
 import utililies.sessionRemote.GestioneLoginRemote;
+import utililies.sessionRemote.GestioneRegistrazioneRemote;
 
 public class LoginTest {
 
 	private static final String MAIL_PEPPINO = "peppino@gmail.com";
-	private static final String PASSWORD = "pippo";
 	private static final String MAIL_UTENTE_INUTILE = "pippo@gmail.com";
 	private static final String PASSWORD_INUTILE = "blabla";
 	private static final String MAIL_AMMINISTRATORE = "admin@swim.it";
+	private static final String PASSWORD = "pippo";
 
 	private GestioneLoginRemote gestioneLogin;
+	private GestioneRegistrazioneRemote gestioneRegistrazione;
+	private static TestUtilsRemote testUtils;
 
 	public LoginTest() throws NamingException {
 		Properties env = new Properties();
@@ -32,41 +36,49 @@ public class LoginTest {
 		Object obj = (new InitialContext(env)).lookup("SWIMv2_EAR/GestioneLogin/remote-utililies.sessionRemote.GestioneLoginRemote");
 		gestioneLogin = (GestioneLoginRemote) PortableRemoteObject.narrow(obj, GestioneLoginRemote.class);
 
+		obj = (new InitialContext(env)).lookup("SWIMv2_EAR/GestioneRegistrazione/remote-utililies.sessionRemote.GestioneRegistrazioneRemote");
+		gestioneRegistrazione = (GestioneRegistrazioneRemote) PortableRemoteObject.narrow(obj, GestioneRegistrazioneRemote.class);
+
+		obj = (new InitialContext(env)).lookup("SWIMv2_EAR/TestUtils/remote-test.TestUtilsRemote");
+		testUtils = (TestUtilsRemote) PortableRemoteObject.narrow(obj, TestUtilsRemote.class);
+	}
+
+	@Before
+	public static void setUpLoginTest() {
+		//testUtils.svuotaTabellaDatabase("Utente"); perche' funzioni bisogna non avere nulla del db, altrimenti viola l'integrita' referenziale
+		//testUtils.svuotaDatabase(); nel caso si voglia svuotare tutto il db prima di fare i test
+
+		//gestioneRegistrazione.registraUtente(MAIL_PEPPINO,PASSWORD);
+		//gestioneRegistrazione.registraAmministratore(MAIL_AMMINISTRATORE, PASSWORD);
 	}
 
 	@Test
 	public void testEseguiLoginUtente() {
-		//per fare il test bisogna prima popolare il db da workbench, in futuro quando jack avra' fatto la registrazione e committata
-		//uso quei metodi per registrare un po' di utenti e usarli per testare
-		
-		//inserire nel db l'utente asas@aaa.it con password a2242ead55c94c3deb7cf2340bfef9d5bcaca22dfe66e646745ee4371c633fc8
+		//inserire nel db l'utente peppino@gmail.com con password a2242ead55c94c3deb7cf2340bfef9d5bcaca22dfe66e646745ee4371c633fc8
 		String passwordHashata = PasswordHasher.hashPassword(PASSWORD);
 
 		//il metodo hasha la password e se trova quella hashata uguale nel db da true
 		Assert.assertTrue(gestioneLogin.esegueLoginUtente(MAIL_PEPPINO, PASSWORD));
-		
+
 		//ovviamente se rihashi una password non e' quella originale
 		Assert.assertFalse(gestioneLogin.esegueLoginUtente(MAIL_PEPPINO, passwordHashata));
-		
+
 		//questo utente non e' presente e allora dice false
 		Assert.assertFalse(gestioneLogin.esegueLoginUtente(MAIL_UTENTE_INUTILE, PASSWORD_INUTILE)); 
 	}
-	
+
 	@Test
 	public void testEseguiLoginAmministratore() {
-		//per fare il test bisogna prima popolare il db da workbench, in futuro quando jack avra' fatto la registrazione e committata
-		//uso quei metodi per registrare un po' di utenti e usarli per testare
-		
-		//inserire nel db l'admin asas@aaa.it con password a2242ead55c94c3deb7cf2340bfef9d5bcaca22dfe66e646745ee4371c633fc8
+		//inserire nel db l'admin admin@swim.it con password a2242ead55c94c3deb7cf2340bfef9d5bcaca22dfe66e646745ee4371c633fc8
 		String passwordHashata = PasswordHasher.hashPassword(PASSWORD);
 
 		//il metodo hasha la password e se trova quella hashata uguale nel db da true
 		Assert.assertTrue(gestioneLogin.eseguiLoginAmministratore(MAIL_AMMINISTRATORE, PASSWORD));
-		
+
 		//ovviamente se rihashi una password non e' quella originale
 		Assert.assertFalse(gestioneLogin.eseguiLoginAmministratore(MAIL_AMMINISTRATORE, passwordHashata));
-		
-		//questo utente non e' presente e allora dice false
+
+		//questo admin non e' presente e allora dice false
 		Assert.assertFalse(gestioneLogin.eseguiLoginAmministratore(MAIL_UTENTE_INUTILE, PASSWORD_INUTILE)); 
 	}
 }
