a28038d378ab89ce7714813c08e2ff1f45af3e2a
bulla.jacopo@gmail.com <bulla.jacopo@gmail.com>
2012-12-28 21:28:26 +0000

    Ho sistemato GestioneRicerche con le exception. Ho anche iniziato a fare il test, ma mi da già errore solo con l'inizializzazione. Non capisco il perchè, ma ci guarderò domani mattina. A meno che ad uno di voi due non venga una illuminazione solo guardandola.

--- SWIMv2_EJB/ejbModule/sessionBeans/interfaces/GestioneRicercheInterface.java
+++ SWIMv2_EJB/ejbModule/sessionBeans/interfaces/GestioneRicercheInterface.java
@@ -4,12 +4,13 @@ import java.util.List;
 
 import entityBeans.Abilita;
 import entityBeans.Utente;
+import exceptions.RicercheException;
 
 public interface GestioneRicercheInterface {
 	
-	public List<Utente> ricercaAiuto(List<Abilita> abilita);
-	public List<Utente> ricercaUtenti(String nome, String cognome);
+	public List<Utente> ricercaAiuto(List<Abilita> abilita) throws RicercheException;
+	public List<Utente> ricercaUtenti(String nome, String cognome) throws RicercheException;
 	public List<Abilita> insiemeAbilitaGenerali();
-	public List<Abilita> insiemeAbilitaPersonaliUtente(String emailUtente);
+	public List<Abilita> insiemeAbilitaPersonaliUtente(String emailUtente) throws RicercheException;
 	
 }
