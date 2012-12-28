0a8f0d9899687f20e0f52f0fff14c54d0099d9bc
daven8989@gmail.com <daven8989@gmail.com@5549e029-e0ad-b569-f86b-1b750b3ebaed>
2012-12-28 22:22:25 +0000

    bozza di GestioneModificaProfilo. tanti dubbi. sul dd non abbiamo scritto se si può cambiare la password.. io lo farei fare, tanto è un metodo del cazzo. dubbi sulla gestione dell'amministratore tolta. ma nessuno mi caga. forse perchè siete tutti in giro a divertirvi. stronzi.

--- trunk/SWIMv2_EJB/ejbModule/sessionBeans/GestioneModificaProfilo.java
+++ trunk/SWIMv2_EJB/ejbModule/sessionBeans/GestioneModificaProfilo.java
@@ -1,16 +1,73 @@
 package sessionBeans;
 
+import java.sql.Blob;
+import java.util.List;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import entityBeans.Abilita;
+import entityBeans.Possiede;
+import entityBeans.PossiedePK;
+import entityBeans.Utente;
+
+import sessionBeans.interfaces.GestioneModificaProfiloInterface;
 import sessionBeans.localInterfaces.GestioneModificaProfiloLocal;
 import utililies.sessionRemote.GestioneModificaProfiloRemote;
 
 @Stateless
-public class GestioneModificaProfilo implements GestioneModificaProfiloLocal, GestioneModificaProfiloRemote {
+public class GestioneModificaProfilo implements GestioneModificaProfiloLocal, GestioneModificaProfiloRemote, GestioneModificaProfiloInterface {
 
 	@PersistenceContext(unitName = "SWIMdb")
 	private EntityManager entityManager;
+	
+	@Override
+	public boolean modificaFoto (String emailUtente, Blob fotoProfilo){
+		Utente utente;
+		
+		utente=this.getUtenteByEmail(emailUtente);
+		if(utente==null){
+			return false;
+		}
+		utente.setNome(emailUtente);
+		
+		entityManager.persist(utente);
+		entityManager.flush();
+		
+		return true;
+	}
+	
+	@Override
+	public boolean modificaSetAbilita (String emailUtente, List<Abilita> abilita){
+		Utente utente;
+		
+		utente=this.getUtenteByEmail(emailUtente);
+		if(utente==null){
+			return false;
+		}
+		
+		for (Abilita a : abilita) {
+			Possiede possiede= new Possiede();
+			PossiedePK possiedepk = new PossiedePK();
+			possiedepk.setAbilita(a);
+			possiedepk.setUtente(utente);
+			possiede.setPossiedePK(possiedepk);
+			
+			entityManager.persist(possiede);
+			entityManager.flush();
+		}
+		entityManager.flush();
+		return true;
+		
+	}
+	
+	
+	
+	@Override
+	public Utente getUtenteByEmail(String email) {
+		Utente utente = entityManager.find(Utente.class, email);
+		return utente;
+	}
 	
 }
--- trunk/SWIMv2_EJB/ejbModule/sessionBeans/interfaces/GestioneModificaProfiloInterface.java
+++ trunk/SWIMv2_EJB/ejbModule/sessionBeans/interfaces/GestioneModificaProfiloInterface.java
@@ -1,5 +1,18 @@
 package sessionBeans.interfaces;
 
+import java.sql.Blob;
+import java.util.List;
+
+import entityBeans.Abilita;
+import entityBeans.Utente;
+
 public interface GestioneModificaProfiloInterface {
+
+	public abstract boolean modificaFoto(String emailUtente, Blob fotoProfilo);
+
+	public abstract boolean modificaSetAbilita(String emailUtente,
+			List<Abilita> abilita);
+
+	public abstract Utente getUtenteByEmail(String email);
 
 }
