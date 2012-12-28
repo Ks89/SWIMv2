b218727ac06a22e8022804f849aaf96defd78d79
Stefano Cappa <stefano.cappa@mail.polimi.it>
2012-12-28 22:22:28 +0000

    WEB: Si ok, che pirla, non potete vederlo perchè è sul mio db, ma potete provarlo facendo la registrazione :)...tolte un po' di log.debug

--- SWIMv2_WEB/src/it/swim/servlet/profilo/FotoServlet.java
+++ SWIMv2_WEB/src/it/swim/servlet/profilo/FotoServlet.java
@@ -12,14 +12,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import lombok.extern.slf4j.Slf4j;
-
 import sessionBeans.localInterfaces.GestioneCollaborazioniLocal;
 
 /**
  * Servlet implementation class FotoServlet
  */
-@Slf4j
 public class FotoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -39,29 +36,13 @@ public class FotoServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String emailUtenteCollegato = (String) UtenteCollegatoUtil.getEmailUtenteCollegato(request);
-
 		if (emailUtenteCollegato == null) {
 			response.sendRedirect("Home");
 			return;
 		}
-		
-		log.debug("++++++++++++++++++++++++++++++++++++++++++++++++ EmailUtente collegato: " + emailUtenteCollegato);
-		
 		try {
-			
-			log.debug("++++++++++++++++++++++++++++++++++++++++++++++++ Utente collegato: " + gestioneCollaborazioni.getUtenteByEmail(emailUtenteCollegato));
-			
 			Blob blob = gestioneCollaborazioni.getUtenteByEmail(emailUtenteCollegato).getFotoProfilo();
-
-			log.debug("++++++++++++++++++++++++++++++++++++++++++++++++ blob length: " + blob.length());
-
-			log.debug("++++++++++++++++++++++++++++++++++++++++++++++++ blob length: " + blob);
-
 			byte[] foto = blob.getBytes(1, (int)blob.length());
-			
-			log.debug("++++++++++++++++++++++++++++++++++++++++++++++++ blob length: " + foto.length);
-
-			
 			response.setContentType("image/jpg");
 			response.getOutputStream().write(foto);
 		} catch(SQLException e) {
