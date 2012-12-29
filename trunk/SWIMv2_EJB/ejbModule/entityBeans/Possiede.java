3cb7a77f2b9457e6c3b24801833b70d69d019375
stefano.cappa.ks89@gmail.com <stefano.cappa.ks89@gmail.com@5549e029-e0ad-b569-f86b-1b750b3ebaed>
2012-12-29 20:51:01 +0000

    Così funziona. Errori: la , dopo l'ultima namedquery non va e poi quando si mette una lista, ho visto su internet che bisogna fare (:lista)  cioè tra "( )". Cmq le query si mettono negli entitybeans e non nelle pk che sono solo classi di supporto. Cmq il problema vero era qualche cosa di sintassi, o la , o le ().

--- trunk/SWIMv2_EJB/ejbModule/entityBeans/Possiede.java
+++ trunk/SWIMv2_EJB/ejbModule/entityBeans/Possiede.java
@@ -4,11 +4,26 @@ import java.io.Serializable;
 
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+@NamedQueries({
+	/*@NamedQuery(name = "PossiedePK.getUtenteByAbilita", query = "SELECT u FROM Utente u WHERE u.email IN " +
+	"(SELECT DISTINCT p.utente " +
+	"FROM PossiedePK p " +
+	"WHERE p.abilita IN :insiemeAbilita and (SELECT COUNT(pk) " +
+											"FROM PossiedePK pk " +
+											"WHERE pk.abilita IN :insiemeAbilita and pk.utente=p.utente) = :numAbilita )"),*/
+			@NamedQuery(name = "Possiede.getUtenteByAbilita",
+					query = "SELECT p.possiedePK.utente FROM Possiede p WHERE p.possiedePK.abilita IN (:insiemeAbilita)"),
+
+			@NamedQuery(name = "Possiede.getAbilitaByUtente",
+					query = "SELECT p.possiedePK.abilita FROM Possiede p WHERE p.possiedePK.utente.email = :emailUtente")
+})
 
 @Data
 @AllArgsConstructor
