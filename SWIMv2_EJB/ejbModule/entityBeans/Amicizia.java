1bd54268ef7dd9fb833ed5c557ed10940112a638
daven8989@gmail.com <daven8989@gmail.com>
2013-01-04 18:18:58 +0000

    ok i metodi che mi ha chiesto K finalmente sono funzionanti e testati. posso iniziare a fare qualcosa di grafica o dovete rompere le palle ancora?? xD

--- SWIMv2_EJB/ejbModule/entityBeans/Amicizia.java
+++ SWIMv2_EJB/ejbModule/entityBeans/Amicizia.java
@@ -28,10 +28,10 @@ import lombok.NoArgsConstructor;
 				+ "WHERE a.amiciziaPK.utente2.email = :emailUtente AND a.dataAccettazione IS NULL"),
 		@NamedQuery(name = "Amicizia.getUtentCheHannoAppenaAccettatoLaTuaRichiestaIndiretta", query = "SELECT a.amiciziaPK.utente2 "
 						+ "FROM Amicizia a "
-						+ "WHERE a.amiciziaPK.utente1.email = :emailUtente AND a.dataAccettazione IS NOT NULL AND a.diretta =:FALSE AND a.notificaAlRichiedente =:FALSE"),
+						+ "WHERE a.amiciziaPK.utente1.email = :emailUtente AND a.dataAccettazione IS NOT NULL AND a.diretta =FALSE AND a.notificaAlRichiedente = FALSE"),
 		@NamedQuery(name = "Amicizia.getUtentCheHannoAppenaAccettatoLaTuaRichiestaDiretta", query = "SELECT a.amiciziaPK.utente2 "
 								+ "FROM Amicizia a "
-								+ "WHERE a.amiciziaPK.utente1.email = :emailUtente AND a.dataAccettazione IS NOT NULL AND a.diretta =:TRUE AND a.notificaAlRichiedente =:FALSE"),
+								+ "WHERE a.amiciziaPK.utente1.email = :emailUtente AND a.dataAccettazione IS NOT NULL AND a.diretta =TRUE AND a.notificaAlRichiedente = FALSE"),
 })
 @Data
 @AllArgsConstructor
