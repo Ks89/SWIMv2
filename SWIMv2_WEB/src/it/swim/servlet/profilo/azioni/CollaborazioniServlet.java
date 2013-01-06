8b4dad4a372f536d46a65edd60acfc9a5a178dc1
daven8989@gmail.com <daven8989@gmail.com>
2013-01-06 14:41:39 +0000

    seee le collaborazioni terminanooooo

--- SWIMv2_WEB/src/it/swim/servlet/profilo/azioni/CollaborazioniServlet.java
+++ SWIMv2_WEB/src/it/swim/servlet/profilo/azioni/CollaborazioniServlet.java
@@ -94,7 +94,62 @@ public class CollaborazioniServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		try {
+			gestioneCollab.terminaCollaborazione(Long.valueOf(request.getParameter("tipo")));
+			
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (LoginException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String emailUtenteCollegato = (String) UtenteCollegatoUtil.getEmailUtenteCollegato(request);
+
+		// se e' null e' perche' l'utente non e' collegato e allora devo fare il
+		// redirect alla home
+		if (emailUtenteCollegato == null) {
+			response.sendRedirect("../home");
+			return;
+		}
+		
+		try {
+			List<Collaborazione> collaborazioniDaTerminare = gestioneCollab.getCollaborazioniDaTerminare(emailUtenteCollegato);
+
+			if(collaborazioniDaTerminare==null) { //TODO attenzione PERCHE' l'ho scritto qui????????????????????????????????????
+				request.setAttribute("erroreGetCollaborazioniDaTerminare", "Impossibile ottenere le collaborazioni");
+			}
+			if(collaborazioniDaTerminare.size()>=1) {
+				request.setAttribute("collaborazioniDaTerminare", collaborazioniDaTerminare);
+			} else {
+				request.setAttribute("nonCiSonoCollaborazioniDaTerminare", "Non ci sono collaborazioni in corso");
+			}
+		} catch (LoginException e) {
+			log.error(e.getMessage(), e);
+			request.setAttribute("erroreGetCollaborazioniDaTerminare", "Impossibile ottenere le collaborazioni in corso");
+		}
+		
+		try {
+			List<Collaborazione> collaborazioniDaRilasciareFeedBack = gestioneCollab.getCollaborazioniCreateFeedbackNonRilasciato(emailUtenteCollegato);
+
+			if(collaborazioniDaRilasciareFeedBack==null) { //TODO attenzione PERCHE' l'ho scritto qui????????????????????????????????????
+				request.setAttribute("erroreGetCollaborazioniSenzaFeedback", "Impossibile ottenere le collaborazioni senza feedback");
+				getServletConfig().getServletContext().getRequestDispatcher("/jsp/utenti/profilo/collaborazioni.jsp").forward(request, response);
+				return;
+			}
+			if(collaborazioniDaRilasciareFeedBack.size()>=1) {
+				request.setAttribute("collaborazioniDaRilasciareFeedBack", collaborazioniDaRilasciareFeedBack);
+			} else {
+				request.setAttribute("collaborazioniDaRilasciareFeedBack", collaborazioniDaRilasciareFeedBack);
+				request.setAttribute("nonCiSonoCollaborazioniSenzaFeedback", "Non ci sono collaborazioni senza feedback");
+			}
+		} catch (LoginException e) {
+			log.error(e.getMessage(), e);
+			request.setAttribute("erroreGetCollaborazioniSenzaFeedback", "Impossibile ottenere le collaborazioni senza feedback");
+		}
+	
+		getServletConfig().getServletContext().getRequestDispatcher("/jsp/utenti/profilo/collaborazioni.jsp").forward(request, response);
+	
 	}
 
 }
